0bee02fb849c0b5327095b6062d3dc46854e1dcf
cr0wg4n <mcm.crw@gmail.com>
2020-09-27 03:16:28 -0400

    semi final version

--- main.py
+++ main.py
@@ -16,9 +16,14 @@ def main():
     webcam.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
     # background = cv2.imread('./backgrounds/pycon.png')
     frames = [
+        Frame(id='thunder', path='./backgrounds/thunder.jpg', width=width, height=height),
         Frame(id='pycon', path='./backgrounds/pycon.png', width=width, height=height),
         Frame(id='moon', path='./backgrounds/moon.jpg', width=width, height=height),
-        Frame(id='time', path='./backgrounds/its_time_to_stop.mp4', width=width, height=height, video=True)
+        Frame(id='time', path='./backgrounds/its_time_to_stop.mp4', width=width, height=height, video=True),
+        Frame(id='error', path='./backgrounds/exp_error.mp4', width=width, height=height, video=True),
+        Frame(id='macizo', path='./backgrounds/macizo.jpg', width=width, height=height),
+        Frame(id='rev', path='./backgrounds/revolucion.jpg', width=width, height=height),
+        Frame(id='hacked', path='./backgrounds/hacked.mp4', width=width, height=height, video=True)
     ]
     image_manager = ImageManager()
     frames = image_manager.load_files(frames)
@@ -26,7 +31,12 @@ def main():
         '<ctrl>+<alt>+a': hot_key_a,
         '<ctrl>+<alt>+s': hot_key_s,
         '<ctrl>+<alt>+d': hot_key_d,
-        '<ctrl>+<alt>+f': hot_key_f,
+        '<ctrl>+<alt>+n': hot_key_n,
+        '<ctrl>+<alt>+t': hot_key_t,
+        '<ctrl>+<alt>+e': hot_key_e,
+        '<ctrl>+<alt>+r': hot_key_r,
+        '<ctrl>+<alt>+m': hot_key_m,
+        '<ctrl>+<alt>+h': hot_key_h,
     })
     listener.start()
     print('data loaded!')
@@ -37,6 +47,13 @@ def main():
             if key == 'a':
                 _, image_frame = webcam.read()
                 background = Frame().get_frame_by_id(frames, 'pycon')
+                if background:
+                    result = image_manager.insert_background_instead_green(image_frame, background.frame)
+                    result = cv2.cvtColor(result, cv2.COLOR_BGR2RGBA)
+                    cam.send(result)
+            elif key == 't':
+                _, image_frame = webcam.read()
+                background = Frame().get_frame_by_id(frames, 'thunder')
                 if background:
                     result = image_manager.insert_background_instead_green(image_frame, background.frame)
                     result = cv2.cvtColor(result, cv2.COLOR_BGR2RGBA)
@@ -57,7 +74,38 @@ def main():
                     result = cv2.cvtColor(result, cv2.COLOR_BGR2RGBA)
                     cam.send(result)
                     cam.sleep_until_next_frame()
-            elif key == 'f':
+            elif key == 'e':
+                video = Frame().get_frame_by_id(frames, 'error')
+                if video:
+                    result = video.frame[count]
+                    count += 1
+                    lenght = len(video.frame)
+                    result = cv2.cvtColor(result, cv2.COLOR_BGR2RGBA)
+                    cam.send(result)
+                    cam.sleep_until_next_frame()
+            elif key == 'h':
+                video = Frame().get_frame_by_id(frames, 'hacked')
+                if video:
+                    result = video.frame[count]
+                    count += 1
+                    lenght = len(video.frame)
+                    result = cv2.cvtColor(result, cv2.COLOR_BGR2RGBA)
+                    cam.send(result)
+                    cam.sleep_until_next_frame()
+            elif key == 'm':
+                _, image_frame = webcam.read()
+                background = Frame().get_frame_by_id(frames, 'macizo')
+                if background:
+                    result = cv2.cvtColor(background.frame, cv2.COLOR_BGR2RGBA)
+                    cam.send(result)
+            elif key == 'r':
+                _, image_frame = webcam.read()
+                background = Frame().get_frame_by_id(frames, 'rev')
+                if background:
+                    result = image_manager.insert_background_instead_green(image_frame, background.frame)
+                    result = cv2.cvtColor(result, cv2.COLOR_BGR2RGBA)
+                    cam.send(result)
+            elif key == 'n':
                 _, image_frame = webcam.read()
                 result = cv2.cvtColor(image_frame, cv2.COLOR_BGR2RGBA)
                 cam.send(result)
@@ -76,9 +124,28 @@ def hot_key_d():
     global key
     key = 'd'
 
-def hot_key_f():
+def hot_key_n():
     global key
-    key = 'f'
+    key = 'n'
+    
+def hot_key_t():
+    global key
+    key = 't'
 
+def hot_key_e():
+    global key
+    key = 'e'
+
+def hot_key_m():
+    global key
+    key = 'm'
+
+def hot_key_r():
+    global key
+    key = 'r'
+
+def hot_key_h():
+    global key
+    key = 'h'
 if __name__ == "__main__":
     main()
